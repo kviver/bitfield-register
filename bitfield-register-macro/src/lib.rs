020788bb635bd4bb62f984c5cc8acd20831cff4c
Mikhail Cheshkov <mcheshkov@gmail.com>
2017-11-17 22:38:48 +0300

    Refactor macro implementation
     * Introduce BitFieldPosition methods len and byte_len
     * Introduce filled_byte and emit_read_single_byte
     * Refactor getters using emit_read_single_byte
     * Add tests on BitFieldPosition methods, emit_read_single_byte and filled_byte

--- bitfield-register-macro/src/lib.rs
+++ bitfield-register-macro/src/lib.rs
@@ -9,11 +9,26 @@ use syn::*;
 
 #[macro_use]
 extern crate quote;
+use quote::Tokens;
 
 #[derive(Debug)]
 enum BitFieldPosition {
     Single(u8),
     Range(std::ops::Range<u8>)
+}
+
+impl BitFieldPosition {
+    pub fn len(&self) -> usize {
+        match self {
+            &BitFieldPosition::Single(_) => 1,
+            &BitFieldPosition::Range(ref range) => (range.end - range.start) as usize
+        }
+    }
+
+    pub fn byte_len(&self) -> usize {
+        let len = self.len();
+        if len % 8 == 0 { len / 8 } else { len / 8 + 1 }
+    }
 }
 
 #[derive(Debug)]
@@ -23,6 +38,51 @@ struct BitField {
     ty: Ty
 }
 
+fn filled_byte(from:u8, to:u8) -> u8 {
+    let mut res = 0;
+    for i in from..to {
+        res |= 1 << i;
+    }
+    return res;
+}
+
+fn emit_read_single_byte(from:Tokens, from_bit:usize, to_bit:usize) -> Tokens {
+    assert!(to_bit - from_bit <= 8);
+
+    let bit_len = (to_bit - from_bit) as u8;
+    let from_byte = (from_bit / 8) as usize;
+    let from_bit_mask = (from_bit % 8) as u8;
+
+    if from_bit % 8 == 0 {
+        let mask = filled_byte(from_bit_mask, from_bit_mask + bit_len);
+        return quote! { #from[#from_byte] & #mask };
+    } else {
+        let second_byte = ((to_bit - 1) / 8) as usize;
+        if second_byte == from_byte {
+            let mask = filled_byte(from_bit_mask, from_bit_mask + bit_len);
+            let offset = from_bit % 8;
+
+            println!("emit_read_single_byte {} & {} >> {}", from_byte, mask, offset);
+
+            return quote! {
+                (#from[#from_byte] & #mask) >> #offset
+            };
+        } else {
+            let first_byte_mask = filled_byte(from_bit_mask, 8);
+            let first_byte_offset = from_bit % 8;
+
+            let second_byte_mask = filled_byte(0, from_bit_mask);
+            let second_byte_offset = 8 - first_byte_offset;
+
+            println!("emit_read_single_byte {} & {} >> {} | {} & {} << {}", from_byte, first_byte_mask, first_byte_offset, second_byte, second_byte_mask, second_byte_offset);
+
+            return quote! {
+                ((#from[#from_byte] & #first_byte_mask) >> #first_byte_offset)
+                | ((#from[#second_byte] & #second_byte_mask) << #second_byte_offset)
+            };
+        }
+    }
+}
 
 fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
     
@@ -57,11 +117,13 @@ fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
 
                 let shift = x % 8;
 
+                let getter_body = emit_read_single_byte(quote! { self.0 }, x as usize, (x+1) as usize);
+
                 impl_body = quote! {
                     #impl_body
 
                     pub fn #getter(&self) -> #ty {
-                        let raw: [u8;1] = [(self.0[#byteidx] & #mask) >> #shift];
+                        let raw: [u8;1] = [#getter_body];
                         return bitfield_register::FromBitfield::from_bitfield(raw);
                     }
 
@@ -74,17 +136,16 @@ fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
             },
             &BitFieldPosition::Range(ref range) => {
                 let from = range.start;
-                let to = range.end; 
+                let to = range.end;
 
-                let value_size: usize = (to - from) as usize / 8 + 1;
-                // let value_bit_size = to - from + 1;
+                let value_byte_len = bitfield.position.byte_len();
 
                 let from_byte: usize = from as usize / 8;
                 let to_byte: usize = to as usize / 8;
 
                 let raw_size = to_byte - from_byte + 1;
 
-                println!("value_byte_size:{}", value_size);
+                println!("value_byte_len:{}", value_byte_len);
                 println!("from_byte:{}, to_byte:{}", from_byte, to_byte);
                 println!("raw_size:{}", raw_size);
 
@@ -97,13 +158,13 @@ fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
                 let bit_end = to % 8;
 
                 let mut setter_body = quote! {
-                    let value_array: [u8;#value_size] = bitfield_register::IntoBitfield::into_bitfield(value);
+                    let value_array: [u8;#value_byte_len] = bitfield_register::IntoBitfield::into_bitfield(value);
                     let mut raw: u8;
                 };
 
                 for i in 0..raw_size {
                     
-                    if i < value_size {
+                    if i < value_byte_len {
                         if bit_shift == 0 {
                             setter_body = quote! { #setter_body
                                  raw = value_array[#i];
@@ -146,35 +207,19 @@ fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
                 }
 
                 let mut getter_body = quote! {
-                    let mut value_array: [u8;#value_size] = [0;#value_size];
+                    let mut value_array: [u8;#value_byte_len] = [0;#value_byte_len];
                 };
 
-                for i in 0..value_size {
-
-                    let i_from_byte = i + from_byte;
-
-                    let mask: u8 = if bit_end == 7 || i != raw_size - 1 {0xff}
-                    else {(1 << (bit_end + 1)) - 1};
-                    
+                for i in 0..value_byte_len {
+                    let from_bit_i = from + 8 * i as u8;
+                    let to_bit_i = u8::min(from_bit_i + 8, to + 1);
+                    let read_byte = emit_read_single_byte(quote! { self.0 }, from_bit_i as usize, to_bit_i as usize);
                     getter_body = quote! { #getter_body
-                        value_array[#i] = self.0[#i_from_byte] >> #bit_shift;
+                        value_array[#i] = #read_byte;
                     };
-
-                    if i < raw_size - 1 {
-                        let i_from_byte = i + from_byte + 1;
-                        if bit_shift != 0 {
-                            getter_body = quote! { #getter_body
-                                value_array[#i] |= self.0[#i_from_byte] << (8 - #bit_shift);
-                            }
-                        }
-                    }
-
-                    if mask != 0xff {
-                        getter_body = quote! { #getter_body
-                            value_array[#i] &= #mask;
-                        };
-                    }
                 }
+
+                println!("getter body {}", getter_body);
 
                 impl_body = quote! {
                     #impl_body
@@ -215,6 +260,8 @@ fn output_struct(name: &Ident, bitfields: &Vec<BitField>) -> quote::Tokens {
 pub fn register(_: TokenStream, input: TokenStream) -> TokenStream {
     let s = input.to_string();
     let ast = parse_derive_input(&s).unwrap();
+
+    println!("{}", s);
 
     let fields = match ast.body {
         Body::Enum(_) => panic!("enum not supported"),
@@ -280,4 +327,132 @@ pub fn register(_: TokenStream, input: TokenStream) -> TokenStream {
     let name = &ast.ident;
 
     return output_struct(name, &bitfields).parse().unwrap();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn position_test() {
+        let pos = BitFieldPosition::Single(0);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Single(1);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Single(7);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Single(8);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Single(9);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Single(15);
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((0..1));
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((0..5));
+
+        assert_eq!(pos.len(), 5);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((0..8));
+
+        assert_eq!(pos.len(), 8);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((0..9));
+
+        assert_eq!(pos.len(), 9);
+        assert_eq!(pos.byte_len(), 2);
+
+        let pos = BitFieldPosition::Range((0..10));
+
+        assert_eq!(pos.len(), 10);
+        assert_eq!(pos.byte_len(), 2);
+
+        let pos = BitFieldPosition::Range((0..15));
+
+        assert_eq!(pos.len(), 15);
+        assert_eq!(pos.byte_len(), 2);
+
+        let pos = BitFieldPosition::Range((1..2));
+
+        assert_eq!(pos.len(), 1);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((1..8));
+
+        assert_eq!(pos.len(), 7);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((1..9));
+
+        assert_eq!(pos.len(), 8);
+        assert_eq!(pos.byte_len(), 1);
+
+        let pos = BitFieldPosition::Range((1..10));
+
+        assert_eq!(pos.len(), 9);
+        assert_eq!(pos.byte_len(), 2);
+    }
+
+    #[test]
+    fn emit_read_single_byte_test() {
+        let from = quote!{arr};
+        let res = emit_read_single_byte(from.clone(), 0, 8);
+        assert_eq!(res, quote!{ #from[0usize] & 255u8 });
+
+        let left_mask : u8 = 0b10000000;
+        let from = quote!{arr};
+        let res = emit_read_single_byte(from.clone(), 7, 8);
+        assert_eq!(res, quote!{ (#from[0usize] & #left_mask) >> 7usize });
+
+        let left_mask : u8 = 0b00011100;
+        let from = quote!{arr};
+        let res = emit_read_single_byte(from.clone(), 2, 5);
+        assert_eq!(res, quote!{ (#from[0usize] & #left_mask) >> 2usize });
+
+
+        let left_mask : u8 = 0b11111110;
+        let right_mask : u8 = 0b00000001;
+        let from = quote!{arr};
+        let res = emit_read_single_byte(from.clone(), 9, 17);
+        assert_eq!(res, quote!{ ((#from[1usize] & #left_mask) >> 1usize) | ((#from[2usize] & #right_mask) << 7usize) });
+    }
+
+    #[test]
+    fn filled_byte_test() {
+        assert_eq!(filled_byte(0,0), 0);
+        assert_eq!(filled_byte(0,1), 0b1);
+        assert_eq!(filled_byte(0,2), 0b11);
+
+        assert_eq!(filled_byte(1,2), 0b10);
+        assert_eq!(filled_byte(1,3), 0b110);
+
+        assert_eq!(filled_byte(6,8), 0b11000000);
+        assert_eq!(filled_byte(7,8), 0b10000000);
+
+        assert_eq!(filled_byte(8,8), 0);
+    }
 }
