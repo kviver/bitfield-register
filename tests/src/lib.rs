d21a7b6acbe036aed53f013fbdd32b42b11da24a
Mikhail Cheshkov <mcheshkov@gmail.com>
2017-12-11 21:30:11 +0300

    Add more tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -883,3 +883,396 @@ mod field_2_5 {
         );
     }
 }
+
+mod field_4_7 {
+    use super::bitfield_register::BitfieldRegister;
+    use super::bitfield_register_macro::register;
+
+    #[register()]
+    struct Field47 {
+        #[bitfield(from = 4, to = 7)]
+        field: u8,
+    }
+
+    #[test]
+    fn field_4_7_test() {
+        test_default!(
+            Field47,
+            expected_value = 0,
+            expected_data = [0;1]
+        );
+
+        test_get!(
+            Field47,
+            from = [0;1],
+            expected_value = 0
+        );
+
+        test_get!(
+            Field47,
+            from = [0b10000;1],
+            expected_value = 1
+        );
+
+        test_get!(
+            Field47,
+            from = [0b11110000;1],
+            expected_value = 0b1111
+        );
+
+        test_get!(
+            Field47,
+            from = [0b00001111;1],
+            expected_value = 0
+        );
+
+        test_get!(
+            Field47,
+            from = [0b00011111;1],
+            expected_value = 0b1
+        );
+
+        test_get!(
+            Field47,
+            from = [0b11111111;1],
+            expected_value = 0b1111
+        );
+
+        test_set!(
+            Field47,
+            from = [0;1],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0;1],
+            value = 1,
+            expected_value = 1,
+            expected_data = [0b10000;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0;1],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11110000;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0;1],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11110000;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b00001111;1],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0b00001111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b00001111;1],
+            value = 1,
+            expected_value = 1,
+            expected_data = [0b00011111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b00001111;1],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b00001111;1],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b11111111;1],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0b00001111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b11111111;1],
+            value = 0b1,
+            expected_value = 0b1,
+            expected_data = [0b00011111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b11111111;1],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;1]
+        );
+
+        test_set!(
+            Field47,
+            from = [0b11111111;1],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;1]
+        );
+    }
+}
+
+mod field_0_7 {
+    use super::bitfield_register::BitfieldRegister;
+    use super::bitfield_register_macro::register;
+
+    #[register()]
+    struct Field07 {
+        #[bitfield(from = 0, to = 7)]
+        field: u8,
+    }
+
+    #[test]
+    fn field_0_7_test() {
+        test_default!(
+            Field07,
+            expected_value = 0,
+            expected_data = [0;1]
+        );
+
+        test_get!(
+            Field07,
+            from = [0;1],
+            expected_value = 0
+        );
+
+        test_get!(
+            Field07,
+            from = [0b1;1],
+            expected_value = 1
+        );
+
+        test_get!(
+            Field07,
+            from = [0b11111111;1],
+            expected_value = 0b11111111
+        );
+
+        test_set!(
+            Field07,
+            from = [0;1],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0;1]
+        );
+
+        test_set!(
+            Field07,
+            from = [0;1],
+            value = 1,
+            expected_value = 1,
+            expected_data = [0b1;1]
+        );
+
+        test_set!(
+            Field07,
+            from = [0;1],
+            value = 0b11111111,
+            expected_value = 0b11111111,
+            expected_data = [0b11111111;1]
+        );
+
+        test_set!(
+            Field07,
+            from = [0b11111111;1],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0;1]
+        );
+
+        test_set!(
+            Field07,
+            from = [0b11111111;1],
+            value = 0b1,
+            expected_value = 0b1,
+            expected_data = [0b00000001;1]
+        );
+
+        test_set!(
+            Field07,
+            from = [0b11111111;1],
+            value = 0b11111111,
+            expected_value = 0b11111111,
+            expected_data = [0b11111111;1]
+        );
+    }
+}
+
+mod field_6_9 {
+    use super::bitfield_register::BitfieldRegister;
+    use super::bitfield_register_macro::register;
+
+    // [0 1 2 3 4 5 6 7] [8 9 10 11 12 13 14 15]
+    // [0 1 2 3 4 5 6 7] [0 1  2  3  4  5  6  7]
+
+    #[register()]
+    struct Field69 {
+        #[bitfield(from = 6, to = 9)]
+        field: u8,
+    }
+
+    #[test]
+    fn field_6_9_test() {
+        test_default!(
+            Field69,
+            expected_value = 0,
+            expected_data = [0;2]
+        );
+
+        test_get!(
+            Field69,
+            from = [0;2],
+            expected_value = 0
+        );
+
+        test_get!(
+            Field69,
+            from = [0b1000000,0],
+            expected_value = 1
+        );
+
+        test_get!(
+            Field69,
+            from = [0b11000000,0b11],
+            expected_value = 0b1111
+        );
+
+        test_get!(
+            Field69,
+            from = [0b00111111,0b11111100],
+            expected_value = 0
+        );
+
+        test_get!(
+            Field69,
+            from = [0b01111111, 0b11111100],
+            expected_value = 0b1
+        );
+
+        test_get!(
+            Field69,
+            from = [0b11111111,0b11111111],
+            expected_value = 0b1111
+        );
+
+        test_set!(
+            Field69,
+            from = [0;2],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0;2]
+        );
+
+        test_set!(
+            Field69,
+            from = [0;2],
+            value = 1,
+            expected_value = 1,
+            expected_data = [0b01000000,0]
+        );
+
+        test_set!(
+            Field69,
+            from = [0;2],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11000000,0b00000011]
+        );
+
+        test_set!(
+            Field69,
+            from = [0;2],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11000000,0b00000011]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b00111111,0b11111100],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0b00111111,0b11111100]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b00111111,0b11111100],
+            value = 1,
+            expected_value = 1,
+            expected_data = [0b01111111,0b11111100]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b00111111,0b11111100],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111,0b11111111]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b00111111,0b11111100],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111,0b11111111]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b11111111;2],
+            value = 0,
+            expected_value = 0,
+            expected_data = [0b00111111,0b11111100]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b11111111;2],
+            value = 0b1,
+            expected_value = 0b1,
+            expected_data = [0b01111111,0b11111100]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b11111111;2],
+            value = 0b1111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;2]
+        );
+
+        test_set!(
+            Field69,
+            from = [0b11111111;2],
+            value = 0b11111,
+            expected_value = 0b1111,
+            expected_data = [0b11111111;2]
+        );
+    }
+}
